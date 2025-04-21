345742b6cf37efc01246724b8b006c88504100fe
asrorulkhopid <asroreal@gmail.com>
2025-04-21 12:18:47 +0800

    update listsSurah's column

--- src/component/app/mushaf/Mushaf.jsx
+++ src/component/app/mushaf/Mushaf.jsx
@@ -22,14 +22,17 @@ const Mushaf = () => {
   if (isError) return <p>Error - {isError}</p>;
 
   return (
-    <div className="pb-4 overflow-y-scroll px-4 sm:px-8 md:px-24 no-scrollbar">
+    <div className="pb-2 overflow-y-scroll px-4 sm:px-8 md:px-24 no-scrollbar">
       <div className="flex flex-col">
         <div className="mt-2 self-center">
           <Label title="Daftar Surah" />
         </div>
-        <div className="mt-2 max-h-full grid grid-flow-col grid-rows-144 sm:grid-rows-57 lg:grid-rows-38 gap-2">
+        <div className="mt-2 columns-1 sm:columns-2 lg:columns-3 gap-2">
           {data?.data.map((surah) => (
-            <div key={surah.nomor} onClick={() => handleOnClick(surah.nomor)}>
+            <div
+              className="mb-2"
+              key={surah.nomor}
+              onClick={() => handleOnClick(surah.nomor)}>
               <ItemSurah
                 surahNumber={surah.nomor}
                 surahName={surah.namaLatin}
